9400967ddca4e7995dd2330e81cfcec4414b43d1
Ninoabela <150281823+Ninoabela@users.noreply.github.com>
2025-03-27 13:23:35 +0800

    asd

--- etms_portal/etms_app/views.py
+++ etms_portal/etms_app/views.py
@@ -7,7 +7,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import os
-import datetime
 from datetime import datetime
 
 
